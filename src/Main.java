705117227cce7ac9fb3a18604e1f433ecb9a0858
nguyentnam1997 <nguyentnam.061297@gmail.com>
2023-10-30 23:33:49 +0700

    123

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
+        //int[] nums = {1,2,3,4};
         //System.out.println(Solution.sum(nums, 0));
 
         //fibonaci
-        System.out.println(Solution.fib(50));
+        //System.out.println(Solution.fib(50));
+
+//        List<Integer> nums = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            nums.add(i + 1);
+//        }
+//        System.out.println(Arrays.toString(nums.toArray()));
+        System.out.println(0 % 1);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Solution {
     static void display(int[] nums, int i) {
         if (i == nums.length) {
@@ -17,5 +22,21 @@ public class Solution {
         else if (n == 1) return 1;
         return fib(n - 1) + fib (n - 2);
     }
+    public int findTheWinner(int n, int k) {
+        List<Integer> nums = createArr(n);
+        if (nums.size() == 1) return nums.get(0);
+        int index = k - 1;
+        for (int number : nums) {
+            if (nums.indexOf(number) % index ) {
 
+            }
+        }
+    }
+    public List<Integer> createArr(int n) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            nums.add(i + 1);
+        }
+        return nums;
+    }
 }
